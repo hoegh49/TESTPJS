01169c00633d688745cdbf4b23b05423cc4664a1
PJ <hoegh49@gmail.com>
2021-01-08 21:49:39 +0100

    Initial check-in of module REPO-PJS

---
+++ REPO-PJS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace REPO_PJS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
